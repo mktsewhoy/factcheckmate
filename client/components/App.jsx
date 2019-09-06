cf64aedd853a5d3081ac864a2276ebbc55b53478
Matthew Sew Hoy <mktsewhoy@hotmail.co.nz>
2019-09-06 21:53:08 +1200

    Reload nodemon & webpack.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 import ErrorMessage from './ErrorMessage'
 import LoadSubreddit from './LoadSubreddit'
 import SubredditList from './SubredditList'
-import Header from './Header'
 import WaitIndicator from './WaitIndicator'
 
 const App = () => (
--- client/components/LoadSubreddit.jsx
+++ client/components/LoadSubreddit.jsx
@@ -5,7 +5,7 @@ import {fetchPosts} from '../actions'
 const LoadSubreddit = ({children, dispatch}) => (
   <div>
     <button onClick={() => dispatch(fetchPosts('newzealand'))}>
-      Fetch Newest Posts
+      Fetch New Posts
     </button>
     {children}
   </div>
